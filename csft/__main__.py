2654e576ffe5e3f2103527a9a4887ecac54c6af6
Yan QiDong <yanqd0@outlook.com>
2017-05-15 18:22:55 +0800

    Add argument location.

--- csft/__main__.py
+++ csft/__main__.py
@@ -5,9 +5,19 @@
 The entry point of csft.
 """
 
+import argparse as ap
+from os.path import isdir
+
+from .csft import print_result
+
 
 def main():
-    print('TODO')
+    parser = ap.ArgumentParser(add_help='add help')
+    parser.add_argument('location', help='the directory to be analyzed')
+    args = parser.parse_args()
+    if not isdir(args.location):
+        raise TypeError('%s is not a directory!', args.location)
+    return print_result(args.location)
 
 
 if __name__ == '__main__':
---
+++ csft/csft.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+"""
+The implementations of csft.
+"""
+
+def print_result(location):
+    print('TODO')
