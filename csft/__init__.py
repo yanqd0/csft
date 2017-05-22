b82b884a8995220a0310da9ff53ae9a1dbd50e0c
Yan QiDong <yanqd0@outlook.com>
2017-05-22 21:20:13 +0800

    Update version to 0.2.0

--- csft/__init__.py
+++ csft/__init__.py
@@ -5,7 +5,7 @@ Count Sizes of File Types
 """
 
 __author__ = 'Yan QiDong'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __email__ = 'yanqd0@outlook.com'
 __url__ = 'https://github.com/yanqd0/csft'
 __copyright__ = 'Copyright (C) 2017 ' + __author__
