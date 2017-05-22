3af03e0d4fdfbc15cc728c1f0182845827688157
Yan QiDong <yanqd0@outlook.com>
2017-05-22 23:57:25 +0800

    Test -V option.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 
+import sys
 from subprocess import check_call
 
 from pytest import raises
 
+import csft
 from csft import __main__ as main
 
 
@@ -12,10 +14,32 @@ def test_call():
 
 
 def test_main():
-    main.main(argv=['.'])
+    assert 0 == main.main(argv=['.'])
 
+
+def test_wrong_path():
     with raises(SystemExit):
         main.main(argv=[])
 
     with raises(TypeError):
         main.main(argv=['path/is/not/a/directory'])
+
+
+class _Buffer(object):
+    def __init__(self):
+        self.buffer = []
+
+    def write(self, text):
+        self.buffer.append(text)
+
+
+def test_show_version():
+    test_buf = _Buffer()
+    test_buf, sys.stdout = sys.stdout, test_buf
+    try:
+        main.main(argv=['-V'])
+    except SystemExit as err:
+        assert 0 == err.code
+    finally:
+        test_buf, sys.stdout = sys.stdout, test_buf
+    assert test_buf.buffer[0].strip() == csft.__version__
