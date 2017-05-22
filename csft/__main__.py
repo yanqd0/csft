c4a0dc9ecc12a82735738fe4b80dc74f991b66d7
Yan QiDong <yanqd0@outlook.com>
2017-05-22 23:37:35 +0800

    Add version option to CLI.

--- csft/__main__.py
+++ csft/__main__.py
@@ -8,11 +8,15 @@ The entry point of csft.
 import argparse as ap
 from os.path import isdir
 
+from . import __name__ as _name
+from . import __version__ as _version
 from .csft import print_result
 
 
 def main(argv=None):
-    parser = ap.ArgumentParser(add_help='add help')
+    """ Execute the application CLI. """
+    parser = ap.ArgumentParser(prog=_name)
+    parser.add_argument('-V', '--version', action='version', version=_version)
     parser.add_argument('path', help='the directory to be analyzed')
     args = parser.parse_args(args=argv)
     if not isdir(args.path):
