ffe609eb2c159b4fc872a94f075fa7edb5b3f9ed
Yan QiDong <yanqd0@outlook.com>
2017-06-01 10:40:50 +0800

    Expose csft2data with try

--- csft/__init__.py
+++ csft/__init__.py
@@ -10,3 +10,17 @@ __email__ = 'yanqd0@outlook.com'
 __url__ = 'https://github.com/yanqd0/csft'
 __copyright__ = 'Copyright (C) 2017 ' + __author__
 __license__ = 'MIT License'
+
+
+def _err_func(err):
+    def _func(*args, **kwargs):
+        raise err
+
+    return _func
+
+
+try:
+    from .csft import csft2data
+except ImportError as err:
+    print(err)
+    csft2data = _err_func(err)
