028a8722df2f6df6ca308430c672e651a57bd771
Yan QiDong <yanqd0@outlook.com>
2017-06-01 10:11:43 +0800

    Update version to 0.2.2

--- csft/__info__.py
+++ csft/__info__.py
@@ -5,7 +5,7 @@ Information about this package.
 """
 
 __author__ = 'Yan QiDong'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __email__ = 'yanqd0@outlook.com'
 __url__ = 'https://github.com/yanqd0/csft'
 __copyright__ = 'Copyright (C) 2017 ' + __author__
