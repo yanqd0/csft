c195f86e39811c38903ff8596b0780283833ecff
Yan QiDong <yanqd0@outlook.com>
2017-06-13 17:33:23 +0800

    Add option --top

--- csft/__main__.py
+++ csft/__main__.py
@@ -13,16 +13,34 @@ from . import __version__ as _version
 from .csft import csft2data
 
 
-def main(argv=None):
-    """ Execute the application CLI. """
+def _positive_int(num):
+    number = int(num)
+    if number <= 0:
+        raise ValueError('%s is not positive!', number)
+    return number
+
+
+def _parse_args(argv):
     parser = ap.ArgumentParser(prog=_name)
     parser.add_argument('-V', '--version', action='version', version=_version)
     parser.add_argument('path', help='the directory to be analyzed')
+    parser.add_argument('--top', type=_positive_int, metavar='N',
+                        help='only display top N results')
+
     args = parser.parse_args(args=argv)
     if not isdir(args.path):
         raise TypeError('%s is not a directory!', args.path)
+    return args
 
+
+def main(argv=None):
+    """Execute the application from CLI."""
+    args = _parse_args(argv)
     data = csft2data(args.path)
+
+    if args.top:
+        data = data.head(args.top)
+
     print(data)
     return 0
 
